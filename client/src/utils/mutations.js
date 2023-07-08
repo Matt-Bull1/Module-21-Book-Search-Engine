c249289f01db99a1bcf9d69ccc19566f4398df65
Matthew Bull <mattbull33@gmail.com>
2023-07-07 23:37:33 -0400

    add user and login mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,33 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser(
+    $email: String!
+    $password: String!
+    $username: String!
+  ) {
+    addUser(
+      email: $email
+      password: $password
+      username: $username
+    ) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
