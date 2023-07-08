b4639ea423a3156d87941fe837504254ed7eba68
Matthew Bull <mattbull33@gmail.com>
2023-07-08 00:22:15 -0400

    fixed type

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -14,7 +14,7 @@ import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
-  const { loading, data } = useQuery(GET_ME);
+  const { loading, data } = useQuery(QUERY_ME);
 
   const [removeBook] = useMutation(REMOVE_BOOK);
   
